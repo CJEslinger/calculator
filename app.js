df789d5e6a425488edf8b03c2076e5b23b428ac8
CJ Eslinger <cjeslinger28@gmail.com>
2022-06-03 18:07:36 -0400

    added the rest of the buttons to javascript

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ currentCalcDisplay.textContent = currentNum;
 //number buttons
 const nums = Array.from(document.querySelectorAll('.numbers'));
 
-//decimal button
-const decimalButton = document.querySelector('#decimal');
-
 //operator buttons
 const addButton = document.querySelector('#add');
 const subtractButton = document.querySelector('#subtract');
@@ -17,7 +14,11 @@ const multiplyButton = document.querySelector('#multiply');
 const divideButton = document.querySelector('#divide');
 const powerButton = document.querySelector('#power');
 
-//operator button event funtions
+//equals button, perens, and decimal
+const equalsButton = document.querySelector('#evaluate');
+const leftPeren = document.querySelector('#left-peren');
+const rightPeren = document.querySelector('#right-peren');
+const decimalButton = document.querySelector('#decimal');
 
 nums.forEach(num => {
     num.addEventListener('click', () => {
