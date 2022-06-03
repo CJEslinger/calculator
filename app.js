9f93a86cc4f343564c2afb0143c8c4b3499f9b66
CJ Eslinger <cjeslinger28@gmail.com>
2022-06-03 19:47:55 -0400

    calculator has functionality for basic logic. It definately can break though

--- app.js
+++ app.js
@@ -5,34 +5,25 @@ let opperationNumPair = [];
 
 //display
 const currentCalcDisplay = document.querySelector('#current-calculation');
-const prevCalcDisplay = document.querySelector('#prev-calculation');
+const pendingCalcDisplay = document.querySelector('#prev-calculation');
 
 //number buttons
 const nums = Array.from(document.querySelectorAll('.numbers'));
 nums.forEach(num => {num.addEventListener('click', () => (addCurrentNumToDisplay(num)))});
 
-/*operator buttons
-const addButton = document.querySelector('#add');
-      addButton.addEventListener('click', () => {continueOpperationLine(addButton);})
-const subtractButton = document.querySelector('#subtract');
-      subtractButton.addEventListener('click', () => {continueOpperationLine(subtractButton);})
-const multiplyButton = document.querySelector('#multiply');
-      multiplyButton.addEventListener('click', () => {continueOpperationLine(multiplyButton);})
-const divideButton = document.querySelector('#divide');
-      divideButton.addEventListener('click', () => {continueOpperationLine(divideButton);})
-const powerButton = document.querySelector('#power');
-      powerButton.addEventListener('click', () => {continueOpperationLine(powerButton);})
-*/
-
+//operator buttons
 const operatorButtons = Array.from(document.querySelectorAll('.operators'));
 operatorButtons.forEach(operator => {
-    operator.addEventListener('click', () => continueOpperationLine(operator))
+    operator.addEventListener('click', () => continueOpperationLine(operator));
 });
-      //equals button, perens, and decimal
+//equals button, perens, and decimal
 const equalsButton = document.querySelector('#evaluate');
+equalsButton.addEventListener('click', evaluate)
 const leftPeren = document.querySelector('#left-peren');
 const rightPeren = document.querySelector('#right-peren');
 const decimalButton = document.querySelector('#decimal');
+
+//backend logic****************************************************
 
 function addCurrentNumToDisplay(numButton){
     currentNum += (numButton.textContent);
@@ -40,31 +31,70 @@ function addCurrentNumToDisplay(numButton){
 }
 
 function continueOpperationLine(operator) {
+    if (opperationNumPair.length == 2) {
+        evaluate();
+    }
     pendingCalc = currentNum;
     currentNum = '';
-    prevCalcDisplay.textContent = pendingCalc;
+    pendingCalcDisplay.textContent = pendingCalc;
     currentCalcDisplay.textContent = currentNum;
     opperationNumPair = [pendingCalc, operator.textContent];
     currentOperationLine.push(opperationNumPair);
     console.log(currentOperationLine);
 }
 
-function addFunction() {
+function evaluate() {
+    if (opperationNumPair[1] == '+'){addFunction()}
+    if (opperationNumPair[1] == '-'){subtractFunction()}
+    if (opperationNumPair[1] == '*'){multiplyFunction()}
+    if (opperationNumPair[1] == '/'){divideFunction()}
+    if (opperationNumPair[1] == 'x^'){powerFunction()}
+    opperationNumPair.pop()
+    opperationNumPair.pop()
+}
 
+function addFunction() {
+    currentNum = parseInt(currentNum);
+    pendingCalc = parseInt(pendingCalc);
+    currentNum = pendingCalc + currentNum;
+    pendingCalc = '';
+    currentCalcDisplay.textContent = `${currentNum}`;
+    pendingCalcDisplay.textContent = '';
 }
 
 function subtractFunction() {
-
+    currentNum = parseInt(currentNum);
+    pendingCalc = parseInt(pendingCalc);
+    currentNum = pendingCalc - currentNum;
+    pendingCalc = '';
+    currentCalcDisplay.textContent = `${currentNum}`;
+    pendingCalcDisplay.textContent = '';
 }
 
 function multiplyFunction() {
-
+    currentNum = parseInt(currentNum);
+    pendingCalc = parseInt(pendingCalc);
+    currentNum = pendingCalc * currentNum;
+    pendingCalc = '';
+    currentCalcDisplay.textContent = `${currentNum}`;
+    pendingCalcDisplay.textContent = '';
 }
 
 function divideFunction() {
-
+    currentNum = parseInt(currentNum);
+    pendingCalc = parseInt(pendingCalc);
+    currentNum = pendingCalc * currentNum;
+    pendingCalc = '';
+    currentCalcDisplay.textContent = `${currentNum}`;
+    pendingCalcDisplay.textContent = '';
 }
 
 function powerFunction() {
-
+    currentNum = parseInt(currentNum);
+    pendingCalc = parseInt(pendingCalc);
+    currentNum = Math.pow(pendingCalc, currentNum);
+    pendingCalc = '';
+    currentCalcDisplay.textContent = `${currentNum}`;
+    pendingCalcDisplay.textContent = '';
 }
+//:)
