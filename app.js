ded73ebb69e04714bd8d98bdfc6aa5c19c2b0248
CJ Eslinger <cjeslinger28@gmail.com>
2022-06-03 18:22:38 -0400

    added structure for all operation functions

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ currentCalcDisplay.textContent = currentNum;
 
 //number buttons
 const nums = Array.from(document.querySelectorAll('.numbers'));
+nums.forEach(num => {num.addEventListener('click', () => (addCurrentNumToDisplay(num)))});
 
 //operator buttons
 const addButton = document.querySelector('#add');
+      addButton.addEventListener('click', () => {addFunction();})
 const subtractButton = document.querySelector('#subtract');
+      subtractButton.addEventListener('click', () => {subtractFunction();})
 const multiplyButton = document.querySelector('#multiply');
+      multiplyButton.addEventListener('click', () => {multiplyFunction();})
 const divideButton = document.querySelector('#divide');
+      divideButton.addEventListener('click', () => {divideFunction();})
 const powerButton = document.querySelector('#power');
+      powerButton.addEventListener('click', () => {powerFunction();})
 
 //equals button, perens, and decimal
 const equalsButton = document.querySelector('#evaluate');
@@ -20,10 +26,29 @@ const leftPeren = document.querySelector('#left-peren');
 const rightPeren = document.querySelector('#right-peren');
 const decimalButton = document.querySelector('#decimal');
 
-nums.forEach(num => {
-    num.addEventListener('click', () => {
-        currentNum += (num.textContent);
-        currentCalcDisplay.textContent = currentNum;
-    })
-});
+addButton.addEventListener('click', () => {addFunction();})
 
+function addCurrentNumToDisplay(numButton){
+    currentNum += (numButton.textContent);
+    currentCalcDisplay.textContent = currentNum;
+}
+
+function addFunction() {
+
+}
+
+function subtractFunction() {
+
+}
+
+function multiplyButton() {
+
+}
+
+function divideButton() {
+
+}
+
+function powerButton() {
+    
+}
